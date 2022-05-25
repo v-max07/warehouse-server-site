88d8b97fb4c7800cac809ceb4d16c3396a5dd6da
v-max07 <vusandebnath09@gmail.com>
2022-05-25 21:16:37 +0600

    set update route

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ async function run() {
             res.send(result);
         });
 
+        //Update
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: updateItem
+            };
+            const result = await inventoryCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
     }
     finally {
 
